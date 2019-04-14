4f6366f2ff56665ae5d6816ac05e00e42cc35cea
konradgnat <konradmgnat@gmail.com>
2019-04-14 12:41:28 -0500

    p

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+# Bare Bones MERN Boilerplate
+
--- index.js
+++ index.js
@@ -9,8 +9,7 @@ if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   app.use(express.static('client/build'));
 
-  // Express will serve the index.html file
-  // if it dosen't recognize the route
+  // Express will serve the index.html file if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
